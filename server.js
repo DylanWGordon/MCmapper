cb43f9f218c3d7946153ef59831beb47f6bed6fc
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-16 09:48:42 -0700

    added import for dotenv

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import postgres from "pg";
+import dotenv from 'dotenv'
 
 const app = express();
 dotenv.config();
-const sql = postgres("postgres://localhost/example_db")
 const PORT = process.env.PORT;
+const dburl = process.env.DATABASE_URL;
+const sql = postgres(`"${dburl}"`)
 
 // app.use(express.static("public"));
 
