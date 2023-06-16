91d7b81e023e4dbff013938f5f177289fa47097c
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-16 15:35:11 -0700

    first attempt at get

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 import express from "express";
-import postgres from "pg";
+import { Pool } from "pg";
 import dotenv from 'dotenv'
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
-const dburl = process.env.DATABASE_URL;
+const URL = process.env.DATABASE_URL;
 
+const client = new Pool({
+    connectionString: `${URL}`
+})
+
+app.use(express.json())
 app.use(express.static("public"));
 
+
+//get all poi
+app.get("/", async (req, res) => {
+    try {
+        const result = await client.query(`SELECT * FROM poi`)
+        res.json(result.rows)
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal server error');
+        console.error('Internal Server Error')
+    }
+})
 
 
 
