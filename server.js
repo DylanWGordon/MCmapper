b49a020c04e10afbfcd4f67baedfe9477aed9135
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-16 09:16:55 -0700

    server.js initial launch

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import postgres from "postgres";
+
+const app = express();
+
+const sql = postgres("postgres://localhost/example_db")
+
+app.use(express.static("public"));
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on Port ${PORT}`);
+});
