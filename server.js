66e515ff81328924b23976bc9ed2775a3e3a8537
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-16 19:38:54 -0700

    Uv1.0 removed [0] index from line 124

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.patch('/poi/:id', (req, res) => {
         if (result.rowCount === '0') {
             res.status(404).send('Not Found')
         } else {
-            res.json(result.rows)
+            res.json(result)
         }
     } catch (err) {
         console.error(err);
