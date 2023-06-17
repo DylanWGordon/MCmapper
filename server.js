b8cbc31cc8838890855d3ab27906c3a7d1f554e4
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-16 18:27:22 -0700

    final changes for dv1.0, protoCv1.4 test

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.delete(`/poi/:id`, async (req, res) => {
         res.status(400).send('Bad Request')
     } else {
         try {
-            const result = await client.query('DELETE FROM poi WHERE poi_id = $1', [id])
-            console.log(result)
+            const result = await client.query('DELETE FROM poi WHERE poi_id = $1 RETURNING *', [id])
+            res.json(result)
         } catch (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
@@ -69,9 +69,8 @@ app.post('/poi', async (req, res) => {
         const y = req.body.npCoord[1]
         const z = req.body.npCoord[2]
 
-        const result = await client.query('INSERT INTO poi(poi_name, biome, kind, coordinates, user_comments) VALUES ($1, $2, $3, ST_MakePoint($4, $5, $6), $7)', [npName, npBio, npKind, x, y, z, npComm]);
-        console.log(result)
-        res.json(result.rows)
+        const result = await client.query('INSERT INTO poi(poi_name, biome, kind, coordinates, user_comments) VALUES ($1, $2, $3, ST_MakePoint($4, $5, $6), $7) RETURNING *', [npName, npBio, npKind, x, y, z, npComm]);
+        res.json(result)
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error')
