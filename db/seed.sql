860f8377658d88e5fa445ccc234aaa21129f35c3
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-17 17:51:17 -0700

    First major frontend commit. Generate all entries for poi in a placeholder div on app load, post new entries from within app.

--- db/seed.sql
+++ db/seed.sql
@@ -3,4 +3,6 @@
 INSERT INTO worlds(world_name, world_user) VALUES ('caves and cliffs', 'Tristan');
 
 INSERT INTO poi(name, biome, kind, x, y, z, comments) VALUES ('Nariton Village', 'Cherry Grove', 'Village', -505, 81, -214, 'First village find on the server!!');
-INSERT INTO poi(name, biome, kind, x, y, z, comments) VALUES ('The Non Village', 'changeme', 'village', -1007, 74, 836, '2 houses, fountain, active golem.. but no villagers?')
+INSERT INTO poi(name, biome, kind, x, y, z, comments) VALUES ('The Non Village', 'changeme', 'village', -1007, 74, 836, '2 houses, fountain, active golem.. but no villagers?');
+
+INSERT INTO poi(name, biome, kind, x, y, z, comments) VALUES ('Squid Lake', 'Frozen Ocean', 'Other', 50, -20, 10, 'Tundra Time! Look at all the squids!');
--- main.js
+++ main.js
@@ -1,5 +1,72 @@
-const API_URL = 'https://mcmap-webservice.onrender.com';
+const API_URL = 'https://corsproxy.io/?https://mcmap-webservice.onrender.com';
+
+function showHiddenEle(ele) {
+    if (ele.style.display !== 'flex') {
+        ele.style.display = 'flex'
+    }
+}
+
+//show addpoi form
+const poiShow = document.getElementById("addpoibtn");
+const addPoiDiv = document.getElementById("addpoi")
+poiShow.addEventListener('click', () => {
+    showHiddenEle(addPoiDiv)
+})
 
 
+//update data func
+async function loader() {
+    const fetchPromise = await fetch(`${API_URL}/poi`);
+    const response = await fetchPromise.json()
+    return response
+}
 
+//Fill Display div 
+async function populateDiv() {
+    const tableDiv = document.getElementById('populateMe');
+    const data = await loader()
+    tableDiv.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+
+        const entry = document.createElement('p')
+        entry.textContent = JSON.stringify(data[i]);
+        tableDiv.append(entry)
+    }
+}
+populateDiv()
+
+
+//Add 1 to poi on submit click
+const clickme = document.getElementById("submitpoi");
+clickme.addEventListener('click', async () => {
+    const name = document.querySelector('#poiname').value;
+    const biome = document.getElementById('biomes').value;
+    const kind = document.getElementById('kind').value;
+    const x = parseInt(document.getElementById('xcoord').value);
+    const y = parseInt(document.getElementById('ycoord').value);
+    const z = parseInt(document.getElementById('zcoord').value);
+    const comment = document.getElementById('comment').value;
+    const postBody = {
+        "name": name,
+        "biome": biome,
+        "kind": kind,
+        "x": x,
+        "y": y,
+        "z": z,
+        "comments": comment,
+    }
+    async function postData() {
+        const response = await fetch(`${API_URL}/poi`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postBody),
+        })
+        return response.json
+    }
+    postData();
+    data = await loader();
+    populateDiv()
+})
 
