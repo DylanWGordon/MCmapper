765eee5d9d2861c485ceac5729e0fdb59af1d5af
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-20 12:43:55 -0700

    cors! 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import postgres from "postgres";
 import pkg from 'pg'
 const { Pool } = pkg
-import cors from cors;
+import cors from 'cors';
 import dotenv from 'dotenv'
 
 app.use(cors())
