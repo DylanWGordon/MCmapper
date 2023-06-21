32ce20a50766db051f78184078c35ec835f99db1
Dylan Gordon <dwgor@Dylans-MacBook-Pro.local>
2023-06-20 19:04:01 -0700

    console logging 5

--- public/main.js
+++ public/main.js
@@ -133,7 +133,7 @@ async function searchData(){
     const searchString = document.getElementById('searchBar').value
     const searchValue = encodeURIComponent(searchString);
     
-    const fetchPromise = await fetch(`${API_URL}/search?term=${searchValue}`, {});
+    const fetchPromise = await fetch(`${API_URL}/search?${searchValue}`, {});
     const response = await fetchPromise.json();
     console.log(response)
     return response
